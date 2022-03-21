7ff757dbbd398fcdfb58de97b141b58ae028d9c3
Irfan Zakaria <irfanzakaria06@webmail.umm.ac.id>
2022-03-21 23:20:23 +0700

    Tugas 1

---
+++ Pertemuan 1/Identitas.java
@@ -0,0 +1,23 @@
+package Pertemuan1;
+
+public class Identitas {
+    public static void main(String[]args){
+        
+        String nama, nim, kelas, alamat, hobi, citacita;
+                
+        nama = "Irfan Zakaria";
+        nim = "201910370311285" ;
+        kelas = "PBO - L";
+        alamat = "Jl. Plaosan Blok H-25";
+        hobi = "Riding";
+        citacita = "Membahagiakan orangtua";
+        
+        System.out.println("Nama      : "+nama);
+        System.out.println("NIM       : "+nim);
+        System.out.println("Kelas     : "+kelas);
+        System.out.println("Alamat    : "+alamat);
+        System.out.println("Hobi      : "+hobi);
+        System.out.println("Cita-cita : "+citacita);
+        
+    }
+}
---
+++ Pertemuan 1/Perulangan.java
@@ -0,0 +1,16 @@
+package Pertemuan1;
+
+public class Perulangan {
+    public static void main (String[]args){
+        
+        for(int i=1; i<=20; i+=2){
+            System.out.print(" "+i);
+        }
+        int i=0;
+        while( i <= 20){
+            System.out.print("\n "+i);
+            
+            i+=2;
+        }
+    }
+}
